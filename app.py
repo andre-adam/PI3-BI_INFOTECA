2b5aab7b161cf10b04605e86d5a1c146d0a6c9cc
andre-adam <andreadam29@gmail.com>
2019-09-22 10:58:40 -0400

    teSTE2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Teste push'),
+    html.H1(children='Teste2 push'),
 
     html.Div(children='''
         TEste
