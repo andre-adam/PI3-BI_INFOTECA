c4d953d8f70b2ad111126c54304756df76795b25
andre-adam <andreadam29@gmail.com>
2019-09-23 13:19:43 -0400

    Teste V2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Teste Jenkins auto-commit'),
+    html.H1(children='Hello World!'),
 
     html.Div(children='''
         TEste
