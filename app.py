666d4492a3dbe80f5918df2edbf0769664f7626e
andre-adam <andreadam29@gmail.com>
2019-09-23 14:11:07 -0400

    agora vai

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello World!'),
+    html.H1(children='Hello World! V2'),
 
     html.Div(children='''
         TEste
