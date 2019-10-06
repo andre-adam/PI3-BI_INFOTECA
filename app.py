dd8001c24c7b8db3e41283114d403851da9ee613
andre-adam <andreadam29@gmail.com>
2019-10-06 19:59:18 -0400

    V2, Callback dos clicks adicionado.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
@@ -7,6 +8,7 @@ import psycopg2
 import http.server
 import socketserver
 from plotly.subplots import make_subplots
+from dash.dependencies import Input, Output
 def consulta(sql):
     try:
         connection = psycopg2.connect(user = "tad",
@@ -32,12 +34,12 @@ def consulta(sql):
 
 # Quantidade de livros emprestado por mês
 resultado = consulta('''
-    select c.mes, SUM(qtd.total) 
+    select c.mes_nome, SUM(qtd.total) 
     from dw.ft_qtdlivrosemprestados qtd
     inner join dw.dim_calendario c on c.sk_dim_calendario = qtd.sk_dim_calendario
     where c.mes <> 9
-    group by c.mes
-    order by 1
+    group by c.mes, c.mes_nome
+    order by c.mes
 ''')
 
 resultadoX = [] 
@@ -47,101 +49,231 @@ for i in resultado:
     resultadoX.append(i[0])
     resultadoY.append(i[1])
 
-resultadoLivro = consulta('''
-    select l.sk_dim_livro, l.nome_titulo, sum(qtd.total)
-    from dw.ft_qtdlivrosemprestados qtd
-    inner join dw.dim_calendario c on c.sk_dim_calendario = qtd.sk_dim_calendario
-    inner join dw.dim_livro l on l.sk_dim_livro = qtd.sk_dim_livro
-    where c.mes <> 9	
-    group by l.sk_dim_livro, l.nome_titulo
-    order by 3 desc
-    limit 10
-''')
+def consultarLivros(mes, area):
+    resultadoLivroLabelsInside = []
+    resultadoLivroValuesInside = []
+    resultadoLivro = consulta('''
+        select l.sk_dim_livro, l.nome_titulo, sum(qtd.total)
+        from dw.ft_qtdlivrosemprestados qtd
+        inner join dw.dim_calendario c on c.sk_dim_calendario = qtd.sk_dim_calendario
+        inner join dw.dim_livro l on l.sk_dim_livro = qtd.sk_dim_livro
+        inner join dw.dim_areaturma a on a.sk_dim_areaturma = qtd.sk_dim_areaturma
+        where c.mes <> 9
+        and (c.mes_nome = '%s' or '%s' = '')
+        and (trim(a.siglaarea) = '%s' or '%s' = '')
+        group by l.sk_dim_livro, l.nome_titulo
+        order by 3 desc
+        limit 10
+    ''' % (mes, mes, area, area))
+    for i in resultadoLivro:
+        resultadoLivroValuesInside.append(i[2])
+        resultadoLivroLabelsInside.append((i[1][:78] + '...') if len(i[1]) > 75 else i[1])
+    return [resultadoLivroLabelsInside, resultadoLivroValuesInside]
 
-resultadoLivroLabels = []
-resultadoLivroValues = []
-for i in resultadoLivro:
-    resultadoLivroValues.append(i[2])
-    resultadoLivroLabels.append(i[1])
+resultadoConsultaLivro = consultarLivros("","")
+resultadoLivroLabels = resultadoConsultaLivro[0]
+resultadoLivroValues = resultadoConsultaLivro[1]
+
 
 resultadoAreaTurma  = consulta('''
-    select t.nomearea, c.mes, SUM(qtd.total) 
+    select t.nomearea, c.mes_nome, SUM(qtd.total), t.siglaarea
     from dw.ft_qtdlivrosemprestados qtd
     inner join dw.dim_calendario c on c.sk_dim_calendario = qtd.sk_dim_calendario
     inner join dw.dim_areaturma t on t.sk_dim_areaturma = qtd.sk_dim_areaturma
     where c.mes <> 9
-    group by c.mes, t.nomearea
-    order by 2
+    group by c.mes, c.mes_nome, t.nomearea, t.siglaarea
+    order by c.mes
 ''')
 
 resultadoAreas = []
 resultadoATGrafico = []
 for w in resultadoAreaTurma:
-    resultadoATGrafico.append({'x':[int(w[1])], 'y': [int(w[2])], 'type':'bar', 'name':str(w[0])})
+    resultadoATGrafico.append({'x':[w[1]], 'y': [int(w[2])], 'type':'bar', 'name':str(w[0])})
     l = True
     for index, a in enumerate(resultadoAreas):
-        print(index)
-        print(a['name'])
         if a['name'] == w[0]:
             l = False
-            a['x'].append(int(w[1]))
+            a['x'].append(w[1])
             a['y'].append(int(w[2]))
     if l:
-        resultadoAreas.append({'x':[int(w[1])], 'y': [int(w[2])], 'type':'bar', 'name':str(w[0])})
+        resultadoAreas.append({'x':[w[1]], 'y': [int(w[2])], 'type':'bar', 'name':str(w[0]), 'sigla': str(w[3])})
 
-graficos = []
-for i in resultadoLivro:
-    graficos.append({'x': [int(i[2])], 'y': [int(i[2])], 'type': 'domain', 'name': i[1]})
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(children=[
     html.H1(style={'text-align':'center'},children='BI InfoTeca'),
-
-    html.Div(children='''
-
-    '''),
-
-    dcc.Graph(
-        id='graph-qtd-mes',
-        figure={
-            'data': [
-                {'x': resultadoX, 'y': resultadoY, 'type': 'bar', 'name': 'Qtd'},
+    html.Div([
+        dcc.Dropdown(
+            id="dropdown-ano",
+            options=[
+                {'label': '2019', 'value': '2019'}
             ],
-            'layout': {
-                'title': 'Quantidade de empréstimo por mês',
+            value='2019',
+            style={
+                'display': 'none'
             }
-        },
-        style={
-            'width': '35%',
-            'display': 'inline-block'
-        }
-    ),
-    dcc.Graph(
-        id='graph-qtd-mes-por-area',
-        figure={
-            'data': resultadoAreas,
-            'layout': {
-                'title': 'Quantidade de empréstimo por mês e turma',
+        ),
+        dcc.Dropdown(
+            id="dropdown-mes",
+            options=[
+                {'label': 'Janeiro', 'value': 'Janeiro'},
+                {'label': 'Fevereiro', 'value': 'Fevereiro'},
+                {'label': 'Março', 'value':'Março'},
+                {'label': 'Abril', 'value': 'Abril'},
+                {'label': 'Maio', 'value': 'Maio'},
+                {'label': 'Junho', 'value':'Junho'},
+                {'label': 'Julho', 'value': 'Julho'},
+                {'label': 'Agosto', 'value': 'Agosto'},
+                {'label': 'Setembro', 'value':'Setembro'},
+                {'label': 'Outubro', 'value': 'Outubro'},
+                {'label': 'Novembro', 'value': 'Novembro'},
+                {'label': 'Dezembro', 'value':'Dezembro'},
+            ],
+            value='Janeiro',
+            style={
+                'display': 'none'
             }
-        },
-        style={
-            'width': '60%',
-            'display': 'inline-block'
-        }
-    ),
-    dcc.Graph(
-        id='graph-qtd-livro',
-        figure={
-            'data': [go.Pie(labels=resultadoLivroLabels, values=resultadoLivroValues)],
-            'layout':{
-                'title': 'Quantidade de livros emprestados no ano'
-            }
-        }
-    ),
+        ),
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(
+                id='graph-qtd-mes',
+                figure={
+                    'data': [
+                        {'x': resultadoX, 'y': resultadoY, 'type': 'bar', 'name': 'Qtd'},
+                    ],
+                    'layout': {
+                        'title': 'Quantidade de empréstimo por mês em 2019',
+                    }
+                }
+            )
+        ], md=5),
+        dbc.Col([
+            dcc.Graph(
+                id='graph-qtd-mes-por-area',
+                figure={
+                    'data': resultadoAreas,
+                    'layout': {
+                        'title': 'Quantidade de empréstimo por mês e área',
+                    }
+                },
+            ),
+        ], md=7,id='col-graph-qtd-mes-por-area')
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(
+                id='graph-qtd-livro',
+                figure={
+                    'data': [go.Pie(labels=resultadoLivroLabels, values=resultadoLivroValues)],
+                    'layout':{
+                        'title': 'Quantidade de empréstimo por livro no ano de 2019'
+                    }
+                }
+            )
+        ], md=12, id="col-graph-qtd-livro")
+    ])
+    ,
 ])
           
+@app.callback(
+    [Output("col-graph-qtd-mes-por-area", "children"),
+     Output("dropdown-mes", "value")
+    ],
+    [Input("graph-qtd-mes", "clickData")]
+)
+def criarGraphQtdMesPorArea(click):
+    grafico = []
+    if click != None:
+        clickX = click["points"][0]["x"].encode('utf-8', 'replace')
+        for i in resultadoAreas:
+            ind = 0
+            for val in i["x"]:
+                if(val[0:3] == clickX[0:3]):
+                    grafico.append({'x':[i["sigla"]], 'y': [i["y"][ind]], 'type':'bar', 'name':str(i["name"]), 'label': str(val)})
+                ind+=1
+        for t in range(0,len(grafico)):
+            for w in range(0, len(grafico)):
+                if(grafico[t]["name"][0:3] < grafico[w-1]["name"][0:3]):
+                    grafico[t],grafico[w-1] = grafico[w-1],grafico[t]
+        return [
+            dcc.Graph(
+                id='graph-qtd-mes-por-area',
+                figure={
+                    'data': grafico,
+                    'layout': {
+                        'title': 'Quantidade de empréstimo por área no mês de ' + clickX
+                    }
+                }
+            ),
+        ], clickX
+    else:
+        return [
+            dcc.Graph(
+                id='graph-qtd-mes-por-area',
+                figure={
+                    'data': resultadoAreas,
+                    'layout': {
+                        'title': 'Quantidade de empréstimo por mês e área',
+                    }
+                },
+            ),
+        ], "Janeiro"
+
+@app.callback(
+    Output("col-graph-qtd-livro", "children"),[
+    Input("graph-qtd-mes-por-area", "clickData"),
+    Input("dropdown-mes", "value")
+    ]
+)
+def criarGraphQtdMesPorLivro(click, mes):
+    mes = mes.encode("utf-8", "replace")
+    if(click != None):
+        if(len(click["points"]) > 1):
+            clickX = click["points"][0]["x"].encode("utf-8", "replace")
+            mes = clickX
+            resultadoConsultaLivro = consultarLivros(clickX,"")
+            resultadoLivroLabels = resultadoConsultaLivro[0]
+            resultadoLivroValues = resultadoConsultaLivro[1]
+            return [dcc.Graph(
+                    id='graph-qtd-livro',
+                    figure={
+                        'data': [go.Pie(labels=resultadoLivroLabels, values=resultadoLivroValues)],
+                        'layout':{
+                            'title': 'Quantidade de empréstimo por livro no ano de 2019 no mês de '+ mes 
+                        }
+                    }
+                )
+            ]
+        else:
+            clickX = click["points"][0]["x"].encode("utf-8", "replace")
+            resultadoConsultaLivro = consultarLivros(mes,clickX)
+            resultadoLivroLabels = resultadoConsultaLivro[0]
+            resultadoLivroValues = resultadoConsultaLivro[1]
+            return [dcc.Graph(
+                    id='graph-qtd-livro',
+                    figure={
+                        'data': [go.Pie(labels=resultadoLivroLabels, values=resultadoLivroValues)],
+                        'layout':{
+                            'title': 'Quantidade de empréstimo por livro no ano de 2019 no mês de '+ mes +' pela área '+ clickX
+                        }
+                    }
+                )
+            ]
+    else:
+        resultadoConsultaLivro = consultarLivros(mes,"")
+        resultadoLivroLabels = resultadoConsultaLivro[0]
+        resultadoLivroValues = resultadoConsultaLivro[1]
+        return [dcc.Graph(
+                id='graph-qtd-livro',
+                figure={
+                    'data': [go.Pie(labels=resultadoLivroLabels, values=resultadoLivroValues)],
+                    'layout':{
+                        'title': 'Quantidade de empréstimo por livro no ano de 2019 no mês de '+ mes
+                    }
+                }
+            )
+        ]
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8080)
